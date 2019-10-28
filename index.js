d6637ad00991cead107abc3e241c31100898f288
Will <comfortwb21@gmail.com>
2019-10-28 12:39:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function moveDodgerLeft() {
+    document.addEventListener("keydown", function(e) {
+        if (e.key === "ArrowLeft") {
+          let leftNumbers = dodger.style.left.replace("px", "");
+          let left = parseInt(leftNumbers, 10);
+       
+          dodger.style.left = `${left - 1}px`;
+        }
+      });
+}
+
+
+function moveDodgerRight() {
+    document.addEventListener("keydown", function(e) {
+        if (e.key === "ArrowRight") {
+          let rightNumbers = dodger.style.right.replace("px", "");
+          let right = parseInt(rightNumbers, 10);
+       
+          dodger.style.right = `${left + 1}px`;
+        }
+      });
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    else if (e.key ==="ArrowRight") {
+        moveDodgerRight()
+    } 
+  });
